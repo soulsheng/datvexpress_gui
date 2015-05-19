d4fc69ff7caa1e17905bfbb2bb935085ecb255e1
S <S>
2015-05-19 16:25:47 +0800

    set_configure , multi packets

--- vc10/main.cpp
+++ vc10/main.cpp
@@ -7,10 +7,23 @@
 void main()
 {
 	DVBS2*	m_dvbs2 = new DVBS2;
+	DVB2FrameFormat	dvbs2_fmt;
+	//
+	// DVB-S2
+	//
+	dvbs2_fmt.frame_type    = FRAME_NORMAL;
+	dvbs2_fmt.code_rate     = CR_1_2;
+	dvbs2_fmt.constellation = M_QPSK;
+	dvbs2_fmt.roll_off      = RO_0_35;
+	dvbs2_fmt.pilots        = 0;
+	dvbs2_fmt.dummy_frame   = 0;
+	dvbs2_fmt.null_deletion = 0;
+	m_dvbs2->s2_set_configure( &dvbs2_fmt );
 
 	u8 b[PACKET_NUMBER*PACKET_SIZE];
 
-	m_dvbs2->s2_add_ts_frame( b );
+	for (int i=0;i<PACKET_NUMBER;i++)
+		m_dvbs2->s2_add_ts_frame( b + i*PACKET_SIZE );
 
 	delete	m_dvbs2;
 }
