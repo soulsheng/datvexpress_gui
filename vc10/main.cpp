9377b64ea8f903d4c4c9cd3c9718b40201430e97
S <S>
2015-05-19 11:05:06 +0800

    encode random info

--- vc10/main.cpp
+++ vc10/main.cpp
@@ -1,7 +1,16 @@
 
 #include "DVBS2.h"
 
+#define PACKET_NUMBER	100
+#define PACKET_SIZE		188
+
 void main()
 {
+	DVBS2*	m_dvbs2 = new DVBS2;
 
+	u8 b[PACKET_NUMBER*PACKET_SIZE];
+
+	m_dvbs2->s2_add_ts_frame( b );
+
+	delete	m_dvbs2;
 }
