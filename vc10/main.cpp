047555aabd903183134abdff03c78350f6174182
S <S>
2015-06-02 14:56:07 +0800

    try to find pl header, no slot like it ( 90 pairs of similar values )

--- vc10/main.cpp
+++ vc10/main.cpp
@@ -7,16 +7,23 @@
 #define CP 0x7FFF
 #define PRINT_SIZE		16
 #define DATA_FROM_ENC	1	// ENCODE OR FILE
+#define VALUE_DIFF		60
 
-#define DATA_FILE_NAME	"s31.1_32apsk_34_long.dat"
+#define DATA_FILE_NAME	"D:\\file\\data\\ldpc\\data_long\\s31.1_16apsk_34_long.dat"
 void init(u8* buffer, int n);	// initialize info
-void print(scmplx* c, int n);	// output encoded info
-void print(u8* b, int n);		// output original info
+void print(scmplx* c, int n, int nstart = 0);	// output encoded info
+template<typename T>
+void print(T* b, int n, int nstart = 0);		// output original info
+int findHeader(scmplx* c, int n, int* pos);
+
+template<typename T>
+int findHeader(T* c, int n, int* pos);
 
 void main()
 {
 	scmplx pl[FRAME_SIZE_NORMAL];
-
+	short  pBuffer[FRAME_SIZE_NORMAL*2];
+	printf("%d,%d,%d \n", sizeof(long), sizeof(int), sizeof(short) );
 #if DATA_FROM_ENC
 	DVBS2*	m_dvbs2 = new DVBS2;
 	DVB2FrameFormat	dvbs2_fmt;
@@ -55,9 +62,29 @@ void main()
 #else
 	FILE *fp = fopen( DATA_FILE_NAME, "rb" );
 	if( fp )
-		fread( pl, sizeof(scmplx), FRAME_SIZE_NORMAL, fp );
+	{
+		fread( pBuffer, sizeof(short), FRAME_SIZE_NORMAL*2, fp );
+		memcpy_s( pl, sizeof(scmplx)*FRAME_SIZE_NORMAL, pBuffer,
+			sizeof(short)*FRAME_SIZE_NORMAL*2 );
+	}
+	else
+		printf("failed to open file %s \n",DATA_FILE_NAME );
+
+	int position[10];
+	int nCan = findHeader( pBuffer, FRAME_SIZE_NORMAL*2, position );
+	printf( "\n candidate count: %d \n", nCan );
+	for( int i=0; i<nCan; i++ ) {
+		int pos = position[i];
+		printf( "\n frame header position is: pl[%d] = (%hd, %hd, %hd) \n", 
+			pos, pBuffer[pos-1], pBuffer[pos], pBuffer[pos+1] );
+		print( pBuffer, PACKET_SIZE, pos );
+		printf("pl[%d]: %hd, %hd \n", pos/2, pl[pos/2].re, pl[pos/2].im );
+	}
+
+
 #endif
 
+	print( pBuffer, PACKET_SIZE );
 	print( pl, PACKET_SIZE );
 
 	DVBS2_DECODE*	m_dvbs2_dec = new DVBS2_DECODE;
@@ -72,17 +99,32 @@ void init(u8* buffer, int n)	// initialize info
 		buffer[i] = i%256;
 }
 
-void print(scmplx* c, int n)	// output encoded info
+void print(scmplx* c, int n, int nstart/* = 0*/)	// output encoded info
 {
-	int nPrint = PRINT_SIZE;//n;
-	for (int i=0;i<nPrint;i++)
+	int nPrint = nstart+PRINT_SIZE;//n;
+	for (int i=nstart;i<nPrint;i++)
 		printf("%d: (%hd,%hd), (%f,%f)\n", i, c[i].re, c[i].im,
 		c[i].re*1.0f/CP, c[i].im*1.0f/CP );
 }
 
-void print(u8* b, int n)		// output original info
+template<typename T>
+void print(T* b, int n, int nstart/* = 0*/)		// output original info
 {
-	int nPrint = PRINT_SIZE;//n;
-	for (int i=0;i<nPrint;i++)
+	int nPrint = nstart+PRINT_SIZE;//n;
+	for (int i=nstart;i<nPrint;i++)
 		printf("%d: %d \n", i, b[i] );
+}
+
+template<typename T>
+int findHeader( T* c, int n, int* pos )
+{
+	int i=1, j=0;
+	int diff = VALUE_DIFF;
+	for (; i<n-1&&j<10; i++)
+		if( abs( abs(c[i]) - abs(c[i-1]) ) < diff && 
+			abs( abs(c[i]) - abs(c[i+1]) ) < diff )
+			pos[j++]=i;
+		else;
+
+	return j;
 }
