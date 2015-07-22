78c9a9b9c1299d39ffba394692caad27c25afc97
S <S>
2015-07-22 16:51:19 +0800

    add driver softBit from template of updateVar, ready to customize

---
+++ fec/driver-softBit.cuh
@@ -0,0 +1,35 @@
+
+#pragma	once
+
+
+class	driverSoftBit
+{
+
+public:
+	bool	launch();		//!	launch kernel 
+
+	bool	verify();	//! verify whether the result is right or wrong
+	
+	driverSoftBit( );
+
+	~driverSoftBit();
+
+private:
+	int nvar, ncheck;
+	int nmaxX1, nmaxX2;
+
+	// host
+	int *sumX1;
+	int *iind;
+	int *mvc, *mcv;
+	int *input;	char *output;
+
+	int *ref_mvc;	char *ref_output;
+
+	// device
+	int *d_sumX1;
+	int *d_iind;
+	int *d_mvc, *d_mcv;
+	int *d_input;	char *d_output;
+
+};
